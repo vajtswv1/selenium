2edced717ac6b5064055db1c14f0c45681bf1a62
ksong <ksong@10.1.10.26>
2017-08-29 10:16:53 -0400

    initial checkin

---
+++ Main/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world!");
+		System.out.println("my first changes");
+
+	}
+
+}
