32ab50c25a63f46ed334f5513127a6718056be12
ksong <ksong@10.1.10.26>
2017-08-29 10:58:34 -0400

    my third changes

--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world!");
 		System.out.println("my first changes");
+		System.out.println("my first changes");
 
 	}
 
